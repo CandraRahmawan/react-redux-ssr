e403958e249c883e2c6efd6ca79b1d04e49eaa58
Candra Rahmawan <candra.rahmawan@rumah123.com>
2018-08-18 12:12:36 +0700

    Add Error Boundary

---
+++ src/clients/components/errors/ErrorBoundary.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Card, CardContent } from '@material-ui/core';
+
+export default class ErrorBoundary extends Component {
+  state = {
+    hasError: false,
+  };
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ backgroundColor: '#2980b9', height: '100vh' }}>
+          <div
+            style={{
+              left: '50%',
+              top: '30%',
+              position: 'absolute',
+              transform: 'translate(-50%, -50%)',
+            }}
+          >
+            <h1
+              style={{
+                textAlign: 'center',
+                fontSize: '250px',
+                marginBottom: '0',
+                color: '#ecf0f1',
+              }}
+            >
+              500
+            </h1>
+            <Card>
+              <CardContent>
+                <h2 style={{ textAlign: 'center' }}>
+                  Oops... Something went wrong !!
+                </h2>
+                <p style={{ color: 'red' }}>
+                  Please check console for error information ...
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
